594c80bd7030ca893fc31c37bf1e23bdfc96c5f2
pranayaryal <drpranayaryal@gmail.com>
2023-05-05 08:30:06 -0400

    add more

--- components/FrontPage.js
+++ components/FrontPage.js
@@ -168,7 +168,7 @@ export const FrontPage = () => {
 
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                     className="w-4 h-4 mt-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                    <path stroke-linecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                   </svg>
                 </div>
 
@@ -339,6 +339,14 @@ export const FrontPage = () => {
           </div>
 
 
+        </div>
+        <div className="flex w-full">
+          <div className="flex flex-col w-[2/3] space-x-0">
+            <h1 className="text-4xl md:text-5xl font-serif">How it works</h1>
+          </div>
+          <div>
+            <p>Image goes here</p>
+          </div>
         </div>
 
       </div>
